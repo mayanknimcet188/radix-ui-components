3d198ae91ced247c341190f4b6329a7b81feb142
Mayank Sharma <56288295+mayanknimcet188@users.noreply.github.com>
2023-01-25 23:43:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import AccordionDemo from "./components/accordion/accordion";
+export default function App() {
+  return (
+    <div className="App">
+      <AccordionDemo />
+    </div>
+  );
+}
